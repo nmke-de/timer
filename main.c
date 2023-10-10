7606176eef9dede0f4a86a7d9d0d3afcdf26e582
nmke-de <github@nmke.de>
2023-10-10 15:29:55 +0200

    Added print_elapsed

--- main.c
+++ main.c
@@ -8,12 +8,33 @@
 time_t start;
 char hours[5];
 char mins[3];
+char secs[3];
+
+void print_elapsed() {
+	time_t elapsed = time(NULL) - start;
+	if (elapsed / 3600 < 10) {
+		strncpy(hours + 1, itoa(elapsed / 3600, 10), 4);
+		hours[0] = '0';
+	}
+	else
+		strncpy(hours, itoa(elapsed / 3600, 10), 5);
+	if ((elapsed % 3600) / 60 < 10) {
+		strncpy(mins + 1, itoa((elapsed % 3600) / 60, 10), 2);
+		mins[0] = '0';
+	}
+	else
+		strncpy(mins, itoa((elapsed % 3600) / 60, 10), 3);
+	if (elapsed % 60 < 10) {
+		strncpy(secs + 1, itoa(elapsed % 60, 10), 2);
+		secs[0] = '0';
+	} else
+		strncpy(secs, itoa(elapsed % 60, 10), 3);
+	printv(" ", hours, ":", mins, ":", secs, "\r");
+}
 
 void fine(int _) {
-	time_t elapsed = time(NULL) - start;
-	strncpy(hours, itoa(elapsed / 3600, 10), 5);
-	strncpy(mins, itoa((elapsed % 3600) / 60, 10), 3);
-	println("\r ", hours, ":", mins, ":", itoa(elapsed % 60, 10), " elapsed.");
+	print_elapsed();
+	println("");
 	_exit(0);
 }
 
@@ -21,18 +42,16 @@ int main() {
 	for (int i = 0; i < 5; i++)
 		hours[i] = 0;
 	for (int i = 0; i < 3; i++)
-		hours[i] = 0;
+		mins[i] = 0;
+	for (int i = 0; i < 3; i++)
+		secs[i] = 0;
 	start = time(NULL);
 	signal(SIGINT, fine);
 	signal(SIGTERM, fine);
 	signal(SIGHUP, fine);
 	signal(SIGQUIT, fine);
 	for (;;) {
-		time_t elapsed = time(NULL) - start;
-		strncpy(hours, itoa(elapsed / 3600, 10), 5);
-		strncpy(mins, itoa((elapsed % 3600) / 60, 10), 3);
-		printv("\r ", hours, ":", mins, ":", itoa(elapsed % 60, 10), " elapsed.");
+		print_elapsed();
 		sleep(1);
 	}
-	// const time_t end = time(NULL);
 }
